692b0446b5f2999a3bc97573c4bf1fddeb65585e
Adam Heinrich <adam@productize.be>
2017-11-28 16:45:55 +0100

    NUCLEO_F413ZH: Add support for the NUCLEO-F413ZH board
    Merged from the existing TARGET_DISCO_F413ZH code (which has the same MCU)
    and TARGET_NUCLEO_F412ZG code (which has the same Nucleo-144 board and
    pinout).

--- tools/build_travis.py
+++ tools/build_travis.py
@@ -58,6 +58,7 @@ build_list = (
     { "target": "NUCLEO_L011K4",     "toolchains": "GCC_ARM", "libs": ["dsp"] },
     { "target": "NUCLEO_L031K6",     "toolchains": "GCC_ARM", "libs": ["dsp"] },
     { "target": "NUCLEO_L073RZ",     "toolchains": "GCC_ARM", "libs": ["dsp"] },
+    { "target": "NUCLEO_F413ZH",     "toolchains": "GCC_ARM", "libs": ["dsp", "usb"] },
     { "target": "NUCLEO_F429ZI",     "toolchains": "GCC_ARM", "libs": ["dsp", "usb"] },
     { "target": "NUCLEO_F446RE",     "toolchains": "GCC_ARM", "libs": ["dsp", "usb"] },
     { "target": "NUCLEO_F446ZE",     "toolchains": "GCC_ARM", "libs": ["dsp", "usb"] },
@@ -190,6 +191,12 @@ linking_list = [
      "toolchains": "GCC_ARM",
      "tests": {""     : ["MBED_2", "MBED_10", "MBED_11", "MBED_16"],
              }
+    },
+    {"target": "NUCLEO_F413ZH",
+     "toolchains": "GCC_ARM",
+     "tests": {""     : ["MBED_2", "MBED_10", "MBED_11", "MBED_16"],
+               "usb"  : ["USB_1", "USB_2" ,"USB_3"],
+               }
     },
     {"target": "NUCLEO_F429ZI",
      "toolchains": "GCC_ARM",
